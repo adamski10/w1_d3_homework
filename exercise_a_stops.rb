a84b023001ea0e22e970f553d50409e3ade99a18
AdamK <adamkoziel@outlook.com>
2020-05-13 18:57:46 +0100

    task 5

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -2,13 +2,19 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 #1. Add "Edinburgh Waverley" to the end of the array
 stops.push("Edinburgh Waverly")
+
 #2. Add "Glasgow Queen St" to the start of the array
 stops.unshift("Glasgow Queen St")
+
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 stops.insert(4, "Polmont")
+
 #4. Print out the index position of "Linlithgow"
 p stops.index("Linlithgow")
+
 #5. Remove "Livingston" from the array using its name
+stops.delete("Livingston")
+
 #6. Delete "Cumbernauld" from the array by index
 #7. Print the number of stops there are in the array?
 #8. Show as many ways as you can to return "Falkirk High" from the array?
